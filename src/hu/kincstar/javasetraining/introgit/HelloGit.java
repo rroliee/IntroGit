963369e50db91cb5d7582b8567a30056101fc2d4
T360-v5-MJA2-s08 <test@email.com>
2020-09-24 15:23:01 +0200

    Init HelloGit

---
+++ src/hu/kincstar/javasetraining/introgit/HelloGit.java
@@ -0,0 +1,9 @@
+package hu.kincstar.javasetraining.introgit;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class HelloGit {
+    public void HelloGit(){
+        System.out.println("Hello Git!");
+    }
+}
